4cf0c2bad52cd699613d390c88cca1b9e3ec69e3
Dinesh Kumar <Dinesh Kumar@DESKTOP-JTUP4PH>
2020-04-30 15:08:52 +0530

    Change

--- bus_booking/src/main/java/com/capgemini/bus_booking/App.java
+++ bus_booking/src/main/java/com/capgemini/bus_booking/App.java
@@ -2,6 +2,15 @@ package com.capgemini.bus_booking;
 
 public class App {
 	public static void main(String[] args) {
+		
 		System.out.println("Hello World");
+	    String h="car";
+		boolean b=h.startsWith("_c");
+		if(h.startsWith("_c")) {
+			System.out.println("Car");
+		}
+		else {
+			System.out.println("Bike");
+		}
 	}
 }
